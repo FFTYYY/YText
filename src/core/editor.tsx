489cfa6d40639e41cfac4c739ca52363c1ade789
Yongyi <1004473299@qq.com>
2022-01-07 01:58:37 +0800

    大师，我悟了

--- src/core/editor.tsx
+++ src/core/editor.tsx
@@ -1,111 +1,32 @@
-/* 
-    这个文件定义了一个React组件，封装了Slate编辑器。
-    exports:
-        YEditor: 一个类，用于管理这个编辑器的自定义元素列表。
-        YEditor.Component: 一个组件，表示一个编辑器。
-    
-    注：
-        Editor对有哪些node一无所知，node需要自己维护在editor中的渲染方式。
-*/
-
-import React from "react";
-import { createEditor , Node , BaseEditor} from 'slate'
-import { Slate, Editable, withReact, ReactEditor} from 'slate-react'
-import { Editor, Transforms } from 'slate'
-import { EditorContext } from "slate-react/dist/hooks/use-slate-static";
-import GroupType from "./elements/group"
-import AbstractType from "./elements/abstract"
-import {paragraph_prototype} from "./meta"
-
-interface YEditorComponentProps{
-    onValueChange: (val: any[]) => any
-    editor: YEditor
-}
-
-interface SlateRendererProps{
-    attributes: any
-    element: Node
-    children: any[]
-}
-
-class _YEditor_Component_ extends React.Component{
-    yeditor: YEditor
-    slate: ReactEditor
-
-    constructor(props: YEditorComponentProps){
-        super(props)
-        this.state = {
-            value: [paragraph_prototype()] , 
-        }
-        this.slate   = props.editor.slate
-        this.yeditor = props.editor
-
-        this.updateValue(this.state.value) //向父组件发送value初始值
-
-        this.slate.isInline = (node) => {
-            return node.flags.has("inline")
-        }
-    }
-
-    renderElement(props: SlateRendererProps){
-        let element = props.element
-        let nodetype = element.type
-        if (nodetype == "paragraph"){
-            return <p {...props.attributes}>{props.children}</p>
-        }
-        else if (nodetype == "group"){
-            let R = this.yeditor.grouptypes[element.groupname].renderer
-            return <R {...props.attributes}>{props.children}</R>
-        }
-        else if (nodetype == "abstract"){
-            let R = this.yeditor.abstractypes[element.abstractname].renderer
-
-            return<R {...props.attributes}>{props.children}</R>
-        }
-        else if(nodetype == "abstract-middle"){
-            return <span {...props.attributes}>{props.children}</span>
-        }
-
-        console.log("type")
-
-        return <div {...props.attributes}>{props.children}</div>
-    }
-
-    updateValue(value:any){
-        this.setState({value:value})
-        if(this.props.onValueChange != undefined){ //将value的值传回给父组件
-            this.props.onValueChange(value)
-        }
-    }
-
-    render(){
-        return <div>
-            <Slate editor={this.slate} value={this.state.value} onChange={value => this.updateValue(value)}>
-                <Editable
-                    renderElement={this.renderElement.bind(this)}
-                />
-            </Slate>
-        </div>
-    }
-
-}
-
-export default class YEditor{
-    grouptypes: any
-    abstractypes: any
-    slate: ReactEditor
-    static Component = _YEditor_Component_
+import type { BaseNode , TextNode , ParagraphNode , GroupNode , StructNode , SupportNode } from "./elements"
+import { TextPrototype , ParagraphPrototype , GroupPrototype , StructPrototype , SupportPrototype } from "./elements"
+class Editor{
+    textstyles: TextStyle[]
 
     constructor(){
-        this.grouptypes = {}
-        this.abstractypes = {}
-        this.slate = withReact(createEditor())
+        this
+    }
+}
+
+class TextStyle{
+    name: string
+    parameter_prototype: any
+    prototype: TextNode
+
+    constructor(name: string , parameter_prototype: any){
+        this.name = name
+        this.parameter_prototype = parameter_prototype
+
+        this.prototype = TextPrototype()
+        this.prototype.name = name
+        this.prototype.parameters = parameter_prototype
     }
 
-    add_grouptype(grouptype: GroupType){
-        this.grouptypes[grouptype.name] = grouptype
+    makenode(){
+        return this.prototype
     }
-    add_abstracttype(abstractype: AbstractType){
-        this.abstractypes[abstractype.name] = abstractype
-    }
+}
+
+class GroupStyle{
+    
 }
---
+++ src/core/elements.tsx
@@ -0,0 +1,106 @@
+/* 这个文件定义所有节点。
+*/
+
+import { Node , BaseElement  } from 'slate'
+
+// 所有可能的节点类型
+type NodeType = "text" | "paragraph" | "group" | "struct" | "support"
+
+// 所有可能的段组连接方式
+type GroupRelationType = "chaining" | "separating"
+
+interface BaseNode{
+    type: NodeType
+    hidden?: BaseNode[]    
+    hidden_parameters?: any
+}
+
+interface TextNode extends BaseNode{
+    text: string
+    name?: string
+    parameters?: string // 文本节点可以有parameters，因为文本节点可以有样式
+}
+
+// 注意段落节点则没有样式，因为所有段落样式都由group定义
+interface ParagraphNode extends BaseNode{
+    children: TextNode[]
+}
+
+interface GroupNode extends BaseNode{
+    name: string
+    parameters: string
+    relation: GroupRelationType
+    children: ParagraphNode[]
+}
+
+interface StructNode extends BaseNode{
+    name: string
+    parameters: string
+    children: GroupNode[] | StructNode[]
+}
+
+interface SupportNode extends BaseNode{
+    name: string
+    parameters: string
+}
+
+function TextPrototype(text: string = ""): TextNode{
+    return {
+        type: "text" , 
+        text: text , 
+    }
+}
+
+function ParagraphPrototype(): ParagraphNode{
+    return {
+        type: "paragraph" , 
+        children: [] , 
+    }
+}
+
+function GroupPrototype(name: string , parameter_proto: any): GroupNode{
+    return {
+        type: "group" , 
+        name: name , 
+
+        parameters: parameter_proto , 
+        relation: "separating" , 
+
+        children: [] , 
+    }
+}
+
+function StructPrototype(name: string , parameter_proto: any): StructNode{
+    return {
+        type: "struct" , 
+        name: name , 
+
+        parameters: parameter_proto , 
+
+        children: [] , 
+    }
+}
+
+function SupportPrototype(name: string , parameter_proto: any): SupportNode{
+    return {
+        type: "support" , 
+        name: name , 
+        parameters: parameter_proto , 
+    }
+}
+
+export {
+    TextPrototype , 
+    ParagraphPrototype , 
+    GroupPrototype , 
+    StructPrototype , 
+    SupportPrototype , 
+}
+export type {
+    BaseNode , 
+    TextNode , 
+    ParagraphNode , 
+    GroupNode , 
+    StructNode , 
+    SupportNode , 
+}
